24abe063aea6671be1c9c6ee1222df6807476583
Raghavendra <raghavendra@doctorite.ai>
2024-05-26 19:24:13 +0530

    Add SVG icons for barrow, send, cam, and refresh

---
+++ src/app/aiChat/page.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import backArrow from '../assets/svg/barrow.svg';
+import Image from 'next/image';
+import refresh from '../assets/svg/refresh.svg';
+import cam from '../assets/svg/cam.svg';
+import send from '../assets/svg/send.svg';
+
+const AiChat = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#F4FBFB]">
+      {/* Header */}
+      <div className="sticky top-0 bg-[#F4FBFB] z-10">
+        <div className="flex justify-between px-3 pt-3">
+          <div>
+            <Image src={backArrow} alt="back" />
+          </div>
+          <div>
+            <Image src={refresh} alt="refresh" />
+          </div>
+        </div>
+        <hr className="border-[0.3px] border-customCyan mt-3" />
+      </div>
+
+      {/* Chat Area */}
+      <div className="flex-grow overflow-y-auto p-3">
+        <div className="space-y-4">
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+          <div>
+            chat area
+            lorem ipsum
+            jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          </div>
+        </div>
+      </div>
+
+      {/* Input Area */}
+      <div className="bg-white flex items-center gap-4 p-3 px-4 shadow-md">
+        <div>
+          <Image src={cam} alt="cam" />
+        </div>
+        <div className="relative flex-grow">
+          <input
+            type="text"
+            placeholder="Type a message"
+            className="w-full bg-gray-100 rounded-full py-2 px-4 text-gray-800 border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 ease-in-out"
+          />
+          <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+            <Image src={send} alt="send" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AiChat;
