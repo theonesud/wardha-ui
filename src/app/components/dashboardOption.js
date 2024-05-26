ec09768899557dddb3c57d2ff4be40165d5a82e2
Raghavendra <raghavendra@doctorite.ai>
2024-05-26 18:10:33 +0530

    chore: Update image size in DashboardOption component

--- src/app/components/dashboardOption.js
+++ src/app/components/dashboardOption.js
@@ -7,7 +7,7 @@ const DashboardOption = ({image, heading, description, buttonText, handleClick})
     <div className='border-0.6 border-customCyan rounded-[0.6px] p-5'>
         <div className='flex items-start justify-start gap-4'>
             <div className=' bg-white p-2 rounded-[4px]'>
-                <img src={image.src} className='w-8 h-6 object-cover' />
+                <img src={image.src} className='w-8 object-cover' />
             </div>
             <div className='flex flex-col gap-1 items-start justify-start'>
                 <div className='text-sm font-light text-customIndigo'>{heading}</div>
