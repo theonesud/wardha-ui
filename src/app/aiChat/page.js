d4bcda24c4f735c99909e6b6ac4c708ce84054c1
Raghavendra <raghavendra@doctorite.ai>
2024-05-30 13:50:17 +0530

    refactor: Update AiChat page layout for improved responsiveness and UI styling

--- src/app/aiChat/page.js
+++ src/app/aiChat/page.js
@@ -226,7 +226,7 @@ const AiChat = ({ searchParams }) => {
   };
 
   return (
-    <div className="h-screen flex flex-col bg-[#F4FBFB]">
+    <div className="h-screen flex justify-between flex-col bg-[#F4FBFB]">
       {isCameraOpen && (
         <CameraCapture
           onCapture={handleCapture}
@@ -334,34 +334,15 @@ const AiChat = ({ searchParams }) => {
           </div>
         ))}
         {loading && (
-          // <div className="rounded-2xl bg-aiChatBg px-4 py-2 mx-5 my-2 max-w-[20%]">
+       
           <Loader />
-          // </div>
+         
         )}
         <div ref={chatEndRef} />
       </div>
 
       {/* Input Area */}
-      {/* <div className="bg-white flex items-center gap-4 p-3 px-4 shadow-md sticky bottom-0">
-        <div onClick={() => setIsCameraOpen(true)}>
-          <Image src={cam} alt="cam" />
-        </div>
-        <div className="relative flex-grow">
-          <input
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            type="text"
-            placeholder="Type a message"
-            className="w-full bg-gray-100 rounded-full py-2 px-4 text-gray-800 border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 ease-in-out"
-          />
-          <div
-            onClick={() => makeApiCall()}
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-          >
-            <Image src={send} alt="send" />
-          </div>
-        </div>
-      </div> */}
+     
       <div  ref={inputAreaRef} className="input-area w-screen flex items-center border border-[#E6E6E6]  justify-center gap-4 py-4 px-5 bg-white">
         <textarea
           ref={textareaRef}
