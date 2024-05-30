74593e0e5d22c6fe6376fbac9989d48438a61a63
Raghavendra <raghavendra@doctorite.ai>
2024-05-30 16:32:32 +0530

    refactor: Update AiChat page layout for improved responsiveness and UI styling

--- src/app/aiChat/page.js
+++ src/app/aiChat/page.js
@@ -249,7 +249,7 @@ const AiChat = ({ searchParams }) => {
   };
 
   return (
-    <div className=" flex justify-between flex-col bg-[#F4FBFB]">
+    <div className={`${textareaRef.current? "h-[40%]": " min-h-screen"} flex justify-between flex-col bg-[#F4FBFB]`}>
       {isCameraOpen && (
         <CameraCapture onCapture={handleCapture} onClose={() => setIsCameraOpen(false)} />
       )}
