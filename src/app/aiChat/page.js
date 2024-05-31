e13a08f2e2e9447b62ba6d76122675b95f7d2d04
Raghavendra <raghavendra@doctorite.ai>
2024-05-31 08:15:24 +0530

    refactor: Improve AiChat page layout for better responsiveness and UI styling

--- src/app/aiChat/page.js
+++ src/app/aiChat/page.js
@@ -29,6 +29,7 @@ const AiChat = ({ searchParams }) => {
   const [isTextAreaFocused, setIsTextAreaFocused] = useState(false);
   const isKeyboardOpen = useDetectKeyboardOpen();
   const [initialFocus, setInitialFocus] = useState(true);
+  
   useEffect(() => {
     const initialMessage = {
       type: 1,
@@ -38,36 +39,33 @@ const AiChat = ({ searchParams }) => {
     setMessagesList([initialMessage]);
   }, []);
 
+  
   useEffect(() => {
     if (textareaRef.current) {
       textareaRef.current.style.height = "auto";
       textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-      if(!searchParams?.isScan){
+      if (!searchParams?.isScan) {
         if (textareaRef.current && initialFocus) {
           textareaRef.current.focus();
           setInitialFocus(false);  // Prevent future auto-focus
         }
       }
-      
-    
+
+
     }
-  }, [message,initialFocus]);
+  }, [message, initialFocus]);
 
   useEffect(() => {
     if (chatEndRef.current) {
       chatEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
-    // if(isKeyboardOpen){
-    //   chatAreaRef.current.scrollIntoView({ behavior: "smooth" });
-    // }
  
+
   }, [messagesList]);
 
   const handleFocus = () => {
     setIsTextAreaFocused(true);
-    // if (chatAreaRef.current) {
-    //   chatAreaRef.current.scrollTop = 0;
-    // }
+ 
   };
 
   const handleBlur = () => {
@@ -78,7 +76,7 @@ const AiChat = ({ searchParams }) => {
     console.log(showResponse, "showResponse");
     if (textareaRef.current) {
       textareaRef.current.blur();
-  }
+    }
     if (showResponse) {
       const res = await fetch("https://walrus-app-hs2a9.ondigitalocean.app/assistant/ask", {
         method: "POST",
@@ -115,7 +113,7 @@ const AiChat = ({ searchParams }) => {
     }
 
     // Close the keyboard
-    
+
   };
 
   const handleRefresh = () => {
@@ -204,152 +202,157 @@ const AiChat = ({ searchParams }) => {
 
   };
 
-  return (
-    <div className={`flex flex-col  bg-[#F4FBFB] ${ isKeyboardOpen ? 'h-[425px] ' : 'h-screen'}`}>
-      {isCameraOpen && (
-        <CameraCapture onCapture={handleCapture} onClose={() => setIsCameraOpen(false)} />
-      )}
-      {/* Header */}
-      <div ref={headerRef} className="header  fixed w-full top-0 bg-[#F4FBFB] z-10">
-        <div className="flex justify-between px-3 pt-3">
-          <div onClick={() => router.push("/dashboard")}>
-            <Image src={backArrow} alt="back" />
-          </div>
-          <div onClick={handleRefresh}>
-            <Image src={refresh} alt="refresh" />
-          </div>
-        </div>
-        <hr className="border-[0.3px] border-customCyan mt-3" />
-      </div>
+  
 
-      {/* Chat Area */}
-      <div
-        ref={chatAreaRef}
-        className=" flex-grow py-[129px] font-sans overflow-y-auto p-3"
-       
-      >
-        {messagesList.map((msg, index) => (
-          <div
-            key={index}
-            className={`flex ${
-              msg.type === 0 ? "justify-end" : "justify-start"
-            } mb-2`}
-            
-          >
-            {msg.type === 1 && (
-              <div className="flex flex-col items-start">
-                <div className="flex gap-4 justify-center items-center font-light">
-                  <div>
-                    <Image src={dot} alt="dot"  />
-                  </div>
-                  <div className="text-xs text-black opacity-60">
-                    Wardah AI Assistant -
-                  </div>
-                </div>
-                {msg.image && msg.image.length > 0 ? (
-                  <div className="flex flex-col gap-2 mx-5 my-2">
-                    <div className="flex flex-row flex-wrap gap-6">
-                      {msg.image.map((img, idx) => (
-                        <Link href={img.url} target="_blank" key={idx}>
-                          <div className="w-[150px] border rounded-md p-2 font-light">
-                            <img
-                              src={img.featured_image}
-                              alt="Product"
-                              width={150}
-                              height={150}
-                            />
-                            <div className="text-[12px] font-medium">
-                              {img.title}
-                            </div>
-                            <div className="text-wrap text-[12px]">
-                              {img.benefits}
-                            </div>
-                          </div>
-                        </Link>
-                      ))}
+  return (
+    <div>
+      <div className={`flex flex-col  bg-[#F4FBFB] ${isKeyboardOpen ? 'h-[430px] ' : 'h-screen'}`}>
+        {isCameraOpen && (
+          <CameraCapture onCapture={handleCapture} onClose={() => setIsCameraOpen(false)} />
+        )}
+        {/* Header */}
+        <div ref={headerRef} className="header  fixed w-full top-0 bg-[#F4FBFB] z-10">
+          <div className="flex justify-between px-3 pt-3">
+            <div onClick={() => router.push("/dashboard")}>
+              <Image src={backArrow} alt="back" />
+            </div>
+            <div onClick={handleRefresh}>
+              <Image src={refresh} alt="refresh" />
+            </div>
+          </div>
+          <hr className="border-[0.3px] border-customCyan mt-3" />
+        </div>
+
+        {/* Chat Area */}
+        <div
+          ref={chatAreaRef}
+          className=" flex-grow  font-sans overflow-y-auto p-3"
+          style={{ paddingTop: '5rem', paddingBottom: '5rem' }}
+        >
+          {messagesList.map((msg, index) => (
+            <div
+              key={index}
+              className={`flex ${msg.type === 0 ? "justify-end" : "justify-start"
+                } mb-2`}
+
+            >
+              {msg.type === 1 && (
+                <div className="flex flex-col items-start">
+                  <div className="flex gap-4 justify-center items-center font-light">
+                    <div>
+                      <Image src={dot} alt="dot" />
+                    </div>
+                    <div className="text-xs text-black opacity-60">
+                      Wardah AI Assistant -
                     </div>
                   </div>
-                ) : (
-                  <div className="rounded-2xl bg-aiChatBg px-4 py-2 mx-5 my-2 max-w-[80%] text-base font-light text-black">
+                  {msg.image && msg.image.length > 0 ? (
+                    <div className="flex flex-col gap-2 mx-5 my-2">
+                      <div className="flex flex-row flex-wrap gap-6">
+                        {msg.image.map((img, idx) => (
+                          <Link href={img.url} target="_blank" key={idx}>
+                            <div className="w-[150px] border rounded-md p-2 font-light">
+                              <img
+                                src={img.featured_image}
+                                alt="Product"
+                                width={150}
+                                height={150}
+                              />
+                              <div className="text-[12px] font-medium">
+                                {img.title}
+                              </div>
+                              <div className="text-wrap text-[12px]">
+                                {img.benefits}
+                              </div>
+                            </div>
+                          </Link>
+                        ))}
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="rounded-2xl bg-aiChatBg px-4 py-2 mx-5 my-2 max-w-[80%] text-base font-light text-black">
+                      {msg.message}
+                    </div>
+                  )}
+
+                  {msg.suggestions && msg.suggestions.length > 0 && (
+                    <div className="flex flex-wrap gap-2 mx-5 max-w-[80%]">
+                      {msg.suggestions.map((suggestion, idx) => (
+                        <SuggestionContainer
+                          key={idx}
+                          text={suggestion}
+                          handleClick={() => makeApiCall(suggestion)}
+                        />
+                      ))}
+                    </div>
+                  )}
+                </div>
+              )}
+
+              {msg.type === 0 && (
+                <div className="flex flex-col items-end overflow-x-hidden">
+                  <div ref={chatEndRef} className="rounded-2xl break-words bg-white px-2 py-2 mx-5 my-2 max-w-[80%] text-base font-light text-black h-auto">
                     {msg.message}
                   </div>
-                )}
-
-                {msg.suggestions && msg.suggestions.length > 0 && (
-                  <div className="flex flex-wrap gap-2 mx-5 max-w-[80%]">
-                    {msg.suggestions.map((suggestion, idx) => (
-                      <SuggestionContainer
-                        key={idx}
-                        text={suggestion}
-                        handleClick={() => makeApiCall(suggestion)}
-                      />
-                    ))}
-                  </div>
-                )}
-              </div>
-            )}
-
-            {msg.type === 0 && (
-              <div className="flex flex-col items-end overflow-x-hidden">
-                <div ref={chatEndRef} className="rounded-2xl break-words bg-white px-2 py-2 mx-5 my-2 max-w-[80%] text-base font-light text-black h-auto">
-                  {msg.message}
+                  {msg.images && msg.images.length > 0 && (
+                    <div className="mt-2">
+                      {msg.images.map((image, idx) => (
+                        <div key={idx} className="w-[150px] p-2 font-light">
+                          <img src={image} alt="Captured" width={150} />
+                        </div>
+                      ))}
+                    </div>
+                  )}
                 </div>
-                {msg.images && msg.images.length > 0 && (
-                  <div className="mt-2">
-                    {msg.images.map((image, idx) => (
-                      <div key={idx} className="w-[150px] p-2 font-light">
-                        <img src={image} alt="Captured" width={150} />
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
-            )}
-          </div>
-        ))}
-        {loading && <Loader />}
+              )}
+            </div>
+          ))}
+          {loading && <Loader />}
+
+        </div>
+
+        {/* Input Area */}
+        <div
+          ref={inputAreaRef}
+          className={`input-area w-screen ${!isKeyboardOpen &&"fixed bottom-0"} flex items-center border border-[#E6E6E6] justify-center gap-4 py-4 px-5 bg-white`}
+        >
+          <textarea
+            ref={textareaRef}
+            value={message}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            onChange={(e) => setMessage(e.target.value)}
+            type="text"
+            placeholder="Type a message"
+            className="w-full h-auto flex-1 outline-none focus:outline-none resize-none bg-none no-scrollbar"
+            rows={1}
+          />
+          {!message ? (
+            <div
+              onClick={() => setIsCameraOpen(true)}
+              className="w-10 h-10 flex justify-center items-center cursor-pointer"
+            >
+              <Image src={cam} alt="cam" />
+            </div>
+          ) : (
+
+            <div
+              onClick={() => {
+                if (textareaRef.current) {
+                  textareaRef.current.blur();
+                }
+                makeApiCall();
+              }}
+              className="w-10 h-10 flex justify-center items-center cursor-pointer"
+            >
+              <Image src={send} alt="send" />
+            </div>
+
+          )}
+        </div>
         
       </div>
-
-      {/* Input Area */}
-      <div
-        ref={inputAreaRef}
-        className={`input-area w-screen ${ !isKeyboardOpen && "fixed bottom-0"} flex items-center border border-[#E6E6E6] justify-center gap-4 py-4 px-5 bg-white`}
-      >
-        <textarea
-          ref={textareaRef}
-          value={message}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          onChange={(e) => setMessage(e.target.value)}
-          type="text"
-          placeholder="Type a message"
-          className="w-full h-auto flex-1 outline-none focus:outline-none resize-none bg-none no-scrollbar"
-          rows={1}
-        />
-        {!message ? (
-          <div
-            onClick={() => setIsCameraOpen(true)}
-            className="w-10 h-10 flex justify-center items-center cursor-pointer"
-          >
-            <Image src={cam} alt="cam" />
-          </div>
-        ) : (
-          
-          <div
-           onClick={() => {
-            if (textareaRef.current) {
-              textareaRef.current.blur();
-            }
-            makeApiCall();
-          }}
-            className="w-10 h-10 flex justify-center items-center cursor-pointer"
-          >
-            <Image src={send} alt="send" />
-          </div>
-          
-        )}
-      </div>
+      
     </div>
   );
 };
